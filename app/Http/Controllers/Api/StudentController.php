c7c174a5780252be4405a6f3ea2837f1d8dd6e43
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-07-03 12:58:21 +0300

    controller update  method

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -92,4 +92,43 @@ class StudentController extends Controller
             ], 404);
         }
     }
+
+    public function update(Request $request, int $id)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'email' => 'required|string|email',
+            'course' => 'required|string|max:191',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => $validator->messages()
+            ], 422);
+        } else {
+            $student = Student::find($id);
+            if ($student) {
+                $student->Update([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'course' => $request->course,
+                    'phone' => $request->phone,
+                ]);
+
+                return response()->json([
+                    'status' => 200,
+                    'student' => $student,
+                    'message' => 'Student Updated successfully!'
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'Student Not Found!'
+                ], 404);
+            }
+        }
+    }
 }
