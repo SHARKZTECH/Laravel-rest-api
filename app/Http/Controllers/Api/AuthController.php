27d92dbe3c9ece914bf44984f53dee12c3c69c5e
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-07-03 16:42:14 +0300

    authcontroler register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            "name" => "required|string",
+            "email" => "required|email|unique:users",
+            "password" => "required|string|confirmed"
+        ]);
+
+        $user = User::create([
+            "name" => $fields["name"],
+            "email" => $fields["email"],
+            "password" => bcrypt($fields["password"]),
+        ]);
+
+        $token = $user->createToken("mytokenapp")->plainTextToken;
+        $response = [
+            "user" => $user,
+            "token" => $token,
+        ];
+        return response()->json($response, 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,6 +9,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post("register", [AuthController::class, 'register']);
+
 
 Route::get("students", [StudentController::class, 'index']);
 Route::post("students", [StudentController::class, 'store']);
