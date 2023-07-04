3ee4e5a35b86d6cd5d6684eda37eceb61bf6117a
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-07-04 05:15:57 +0300

    protected routes

--- routes/api.php
+++ routes/api.php
@@ -10,15 +10,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Protected Routes
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::post("/logout", [AuthController::class, 'logout']);
+
+    Route::post("/students", [StudentController::class, 'store']);
+    Route::put("/students/{id}", [StudentController::class, 'update']);
+    Route::delete("/students/{id}", [StudentController::class, 'destroy']);
+});
+
+
 Route::post("/register", [AuthController::class, 'register']);
 Route::post("/login", [AuthController::class, 'login']);
-Route::post("/logout", [AuthController::class, 'logout']);
-
-
 
 Route::get("/students", [StudentController::class, 'index']);
-Route::post("/students", [StudentController::class, 'store']);
 Route::get("/students/{id}", [StudentController::class, 'show']);
 Route::get("/students/{id}/edit", [StudentController::class, 'edit']);
-Route::put("/students/{id}", [StudentController::class, 'update']);
-Route::delete("/students/{id}", [StudentController::class, 'destroy']);
