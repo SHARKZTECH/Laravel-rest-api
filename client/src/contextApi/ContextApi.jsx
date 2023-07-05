4c0efe79b4cb80e1cdd353b80174325c30c3c132
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-07-05 12:41:52 +0300

    handle get students

--- client/src/contextApi/ContextApi.jsx
+++ client/src/contextApi/ContextApi.jsx
@@ -9,6 +9,7 @@ export const contextApi=createContext();
   const [errMsg,setErrMsg]=useState(null);
   const [user,setUser]=useState(null);
   const [token,setToken]=useState("4|UEzJkXq0ID2Jomils8vBmqEmCX3MV7WTWcamcQkn");
+  const [students,setStudents]=useState(null);
 
     //Register Form
     const initialFormDataRegister = {
@@ -58,10 +59,27 @@ export const contextApi=createContext();
         }  
         
       };
+
+      //Get All Students
+      const getStudents=async()=>{
+        try{
+          const res=await axios.get("students");
+          setStudents(res.data?.students);  
+          
+        }catch(err){
+          setErrMsg(err.response?.data.errors);
+        }  
+      }
+
+      
       
       console.log(errMsg)
 
-    return <contextApi.Provider value={{user,token,handleSubmitRegister,formDataRegister,setFormDataRegister,handleSubmitLogin,formDataLogin,setFormDataLogin}}>{children}</contextApi.Provider>
+    return <contextApi.Provider
+     value={{user,token,students,getStudents,
+      handleSubmitRegister,formDataRegister,setFormDataRegister,
+      handleSubmitLogin,formDataLogin,setFormDataLogin}}
+     >{children}</contextApi.Provider>
 }
 
 export default ContextProvider;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import Students from '../components/Students'
 import AddStudent from '../components/AddStudent'
+import { contextApi } from '../contextApi/contextApi'
 
 const HomePage = () => {
+  const {getStudents} =useContext(contextApi);
+
+  useEffect(()=>{
+    getStudents();
+  },[])
   return (
     <div className='container mx-auto app mt-16'>
         <Students/>
