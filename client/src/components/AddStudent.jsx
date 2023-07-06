a91bec8d4dab72104ae890a657432404fa8e485b
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-07-06 10:41:09 +0300

    add form get input fix

--- client/src/components/AddStudent.jsx
+++ client/src/components/AddStudent.jsx
@@ -1,86 +1,104 @@
-'use client';
-
-import { Button, Checkbox, Label, Modal, TextInput } from 'flowbite-react';
-import { useState } from 'react';
+import { Button, Label, Modal, TextInput } from 'flowbite-react';
+import { useState, useRef } from 'react';
 
 export default function AddStudent() {
   const [openModal, setOpenModal] = useState();
   const props = { openModal, setOpenModal };
 
-  const initialStudentFormData = {
-    name: '',
-    email: '',
-    course: '',
-    phone: '',
-  };
-  const [formDataStudent, setFormDataStudent] = useState(initialStudentFormData);
+  const nameInputRef = useRef(null);
+  const emailInputRef = useRef(null);
+  const courseInputRef = useRef(null);
+  const phoneInputRef = useRef(null);
 
-  const handleInputChange = (e) => {
-    const { id, value } = e.target;
-    setFormDataStudent((prevFormData) => ({
-      ...prevFormData,
-      [id]: value,
-    }));
-  };
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const inputData={
+      name: nameInputRef.current.value,
+      email: emailInputRef.current.value,
+      course: courseInputRef.current.value,
+      phone: phoneInputRef.current.value,
+    };
+    
+    console.log(inputData);
 
-    // Now you can use the formData values to add the student
-    console.log('Name:', formData.name);
-    console.log('Email:', formData.email);
-    console.log('Course:', formData.course);
-    console.log('Phone:', formData.phone);
+    nameInputRef.current.value=null;
+    emailInputRef.current.value=null;
+    courseInputRef.current.value=null;
+    phoneInputRef.current.value=null;
 
-    // Clear the input fields after adding the student
-    setFormDataStudent(initialFormData);
 
-    // Close the modal after form submission
     props.setOpenModal(undefined);
   };
 
   return (
     <>
-      <Button onClick={() => props.setOpenModal('form-elements')}>Add Student</Button>
-      <Modal show={props.openModal === 'form-elements'} size="md" popup onClose={() => props.setOpenModal(undefined)}>
+      <div className='mt-10 flex justify-center'>
+        <Button onClick={() => props.setOpenModal('form-elements')}>Add Student</Button>
+      </div>
+      <Modal
+        show={props.openModal === 'form-elements'}
+        size="xl"
+        popup
+        onClose={() => props.setOpenModal(undefined)}
+      >
         <Modal.Header />
         <Modal.Body>
           <div className="space-y-6">
-            <h3 className="text-xl font-medium text-gray-900 dark:text-white">Add Student</h3>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="name" value="Name" />
+            <h3 className="text-xl font-medium text-gray-900 dark:text-white text-center">Add Student</h3>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="name" value="Name" />
+                </div>
+                <TextInput
+                  id="name"
+                  placeholder="name"
+                  required
+                  ref={nameInputRef}
+                />
               </div>
-              <TextInput id="name" placeholder="name" required onChange={handleInputChange} value={formDataStudent.name}/>
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="email" value="Email" />
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="email" value="Email" />
+                </div>
+                <TextInput
+                  id="email"
+                  placeholder="name@company.com"
+                  required
+                  ref={emailInputRef}
+                />
               </div>
-              <TextInput id="email" placeholder="name@company.com" required onChange={handleInputChange} value={formDataStudent.email}/>
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="course" value="Course" />
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="course" value="Course" />
+                </div>
+                <TextInput
+                  id="course"
+                  placeholder="course"
+                  required
+                  ref={courseInputRef}
+                />
               </div>
-              <TextInput id="course" placeholder="course" required onChange={handleInputChange} value={formDataStudent.course} />
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="phone" value="Phone" />
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="phone" value="Phone" />
+                </div>
+                <TextInput
+                  id="phone"
+                  placeholder="Phone"
+                  required
+                  ref={phoneInputRef}
+                />
               </div>
-              <TextInput id="phone" placeholder="phone" required onChange={handleInputChange} value={formDataStudent.phone} />
-            </div>    
-     
-            <div className="w-full">
-              <Button>Add Student</Button>
-            </div>
-      
+              <div className="w-full">
+                <Button type="submit">Add Student</Button>
+              </div>
+            </form>
           </div>
         </Modal.Body>
       </Modal>
     </>
-  )
+  );
 }
-
-
